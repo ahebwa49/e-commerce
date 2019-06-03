0a938d09d009358619ea64670ecb24da68d37527
Livingstone Asabahebwa <ahebwa49@gmail.com>
2019-06-03 16:03:56 +0300

    add enzyme for testing

--- frontend/__tests__/item.test.js
+++ frontend/__tests__/item.test.js
@@ -1,17 +1,18 @@
 import Item from "../components/Item";
 import { shallow } from "enzyme";
+import toJSON from "enzyme-to-json";
 
 const fakeItem = {
   id: "ABCD",
   title: "A cool item",
-  price: 5000,
+  price: 4000,
   description: "This is really cool",
   image: "dog.jpg",
   largeImage: "largedog.jpg"
 };
 
 describe("<Item />", () => {
-  it("renders and displays title and pricetag properly", () => {
+  /*it("renders and displays title and pricetag properly", () => {
     const wrapper = shallow(<Item item={fakeItem} />);
     const PriceTag = wrapper.find("PriceTag");
     console.log(wrapper.debug());
@@ -25,5 +26,9 @@ describe("<Item />", () => {
     console.log(img.debug());
     expect(img.props().src).toBe(fakeItem.image);
     expect(img.props().alt).toBe(fakeItem.title);
+  });*/
+  it("renders and matches the snapshot", () => {
+    const wrapper = shallow(<Item item={fakeItem} />);
+    expect(toJSON(wrapper)).toMatchSnapshot();
   });
 });
