327d983fc4a1110478fc8c50f9b6557623e1d26b
Livingstone Asabahebwa <ahebwa49@gmail.com>
2019-06-27 19:37:17 +0300

    change the orderBy argument

--- frontend/components/Items.js
+++ frontend/components/Items.js
@@ -8,7 +8,7 @@ import { perPage } from "../config";
 
 const ALL_ITEMS_QUERY = gql`
   query ALL_ITEMS_QUERY($skip:Int = 0, $first:Int=${perPage}) {
-    items(first:$first, skip:$skip, orderBy:createdAt_ASC) {
+    items(first:$first, skip:$skip, orderBy:price_ASC) {
       id
       title
       description
